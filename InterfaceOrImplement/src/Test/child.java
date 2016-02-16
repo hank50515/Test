22bc8d01322a990c0a7f2a72541146f1d29c0dd0
hank50515 <hank50515@yahoo.com.tw>
2016-02-16 09:24:04 +0800

    update
    update

---
+++ InterfaceOrImplement/src/Test/child.java
@@ -0,0 +1,13 @@
+package Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class child extends parent implements interfacee {
+
+    @Override
+    public String teees() {
+        return "123";
+    }
+    List<String> abc = new ArrayList<>();
+}
