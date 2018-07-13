cf275f8d95f40ccf0abe95532f19686ff1eeb3de
hank_lin <hank_lin@mail.gss.com.tw>
2018-07-13 14:12:16 +0800

    test
    test

--- gcs/A.cs
+++
@@ -1,6 +0,0 @@
-class A<U> : MyGenericClass<U> where U: B
-{ 
-    public static int AddNumber<T>(int val1, int val2) where T: C{
-        return val1 + val2;
-    };
-}
--- gcs/D.cs
+++
@@ -1,7 +0,0 @@
-public interface D<T, U>
-        where T : B,
-        where U : C
-{
-    IEnumerable<T> List();
-    T Get(U id);
-}
