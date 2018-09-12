b9f12182454a5cd7a0eb4d1241baf3cdb04507bc
hank_lin <hank_lin@mail.gss.com.tw>
2018-09-12 09:38:58 +0800

    test
    test

---
+++ dblinkTest/native.cs
@@ -0,0 +1,81 @@
+﻿using GSS.Radar.Domain.Models.Mail;
+using GSS.Radar.Domain.Models.Payroll;
+using GSS.Radar.Domain.Models.TaxReturn;
+using GSS.Radar.Infrastructure.Arguments;
+using GSS.Radar.Infrastructure.Data;
+using GSS.Radar.Infrastructure.Data.Impl;
+using GSS.Radar.Infrastructure.Data.SqlHelper;
+using System;
+using System.Collections.Generic;
+　
+namespace GSS.Radar.Core.Services.Domain.Persistence.Impl
+{
+    public class MailInformationDao : EntityDao<MailInformation>, IMailInformationDao
+    {
+        public virtual IList<DbLinkTable> GetDbLinkTable(SQLQueryParameter filter)
+        {
+　
+            SqlBuilder sb = new SqlBuilder(@"
+                        select *
+                        from adm..dao.PROJECT "
+                , SqlBuilder.ParamType.NHibernate)
+                .WhereScope(where =>
+                {
+                    where
+                        .AddWhereClause(filter.FilterGroup)
+                    ;
+                });
+　
+            ExecuteSQL<DbLinkTable>(sb);
+        }
+		
+		public virtual IList<DbLinkTable> GetDbLinkTableByOpenQuery(SQLQueryParameter filter)
+        {
+　
+            SqlBuilder sb = new SqlBuilder(@"
+                        select *
+                        from OPENQUERY(adm, 'SELECT id FROM TEST.PROJECT') "
+                , SqlBuilder.ParamType.NHibernate)
+                .WhereScope(where =>
+                {
+                    where
+                        .AddWhereClause(filter.FilterGroup)
+                    ;
+                });
+　
+            ExecuteSQL<DbLinkTable>(sb);
+        }
+　
+        public virtual IList<DbLinkTable> GetDbLinkTableByInformix(SQLQueryParameter filter)
+        {
+            SqlBuilder sb = new SqlBuilder(@"
+                        select *
+                        from adm@remoteoffice:project "
+                , SqlBuilder.ParamType.NHibernate)
+                .WhereScope(where =>
+                {
+                    where
+                        .AddWhereClause(filter.FilterGroup)
+                    ;
+                });
+　
+            ExecuteSQL<DbLinkTable>(sb);
+        }
+		
+		public virtual IList<DbLinkTable> GetDbLinkTableByInformixAlias(SQLQueryParameter filter)
+        {
+            SqlBuilder sb = new SqlBuilder(@"
+                        select *
+                        from adm@remoteoffice:informix.project "
+                , SqlBuilder.ParamType.NHibernate)
+                .WhereScope(where =>
+                {
+                    where
+                        .AddWhereClause(filter.FilterGroup)
+                    ;
+                });
+　
+            ExecuteSQL<DbLinkTable>(sb);
+        }
+	}
+}
