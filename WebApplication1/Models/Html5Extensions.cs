c3b82dedf98cad355bdc51cd04611ec5e8fc4eaa
hank_lin <hank_lin@mail.gss.com.tw>
2018-10-15 09:47:39 +0800

    test
    test

---
+++ WebApplication1/Models/Html5Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+public static class Html5Extensions
+{
+    public static IHtmlString GetMyName(this Html5Helper html)
+    {
+        var tagBuilder = new TagBuilder("span");
+        tagBuilder.SetInnerText("Hello World");
+        return new HtmlString(tagBuilder.ToString());
+    }
+}
---
+++ WebApplication1/Models/Html5Helper.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+public class Html5Helper
+{
+    public Html5Helper(ViewContext viewContext,
+    IViewDataContainer viewDataContainer)
+    : this(viewContext, viewDataContainer, RouteTable.Routes)
+    {
+    }
+    public Html5Helper(ViewContext viewContext,
+    IViewDataContainer viewDataContainer, RouteCollection routeCollection)
+    {
+        ViewContext = viewContext;
+        ViewData = new ViewDataDictionary(viewDataContainer.ViewData);
+    }
+    public ViewDataDictionary ViewData
+    {
+        get;
+        private set;
+    }
+    public ViewContext ViewContext
+    {
+        get;
+        private set;
+    }
+}
