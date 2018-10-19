62e19ec68e3911c79877b4ddadd0317484409b00
hank_lin <hank_lin@mail.gss.com.tw>
2018-10-19 10:38:30 +0800

    test
    test

---
+++ WebApplication1/Areas/project/projectAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication1.Areas.project
+{
+    public class projectAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "project";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "project_default",
+                "project/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebApplication1/Models/Html5Helper.cs
@@ -0,0 +1,35 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+public class Html5Helper
+{
+    public Html5Helper(ViewContext viewContext,
+    IViewDataContainer viewDataContainer)
+    : this(viewContext, viewDataContainer, RouteTable.Routes)
+    {
+    }
+    public Html5Helper(ViewContext viewContext,
+    IViewDataContainer viewDataContainer, RouteCollection routeCollection)
+    {
+        ViewContext = viewContext;
+        ViewData = new ViewDataDictionary(viewDataContainer.ViewData);
+    }
+    public ViewDataDictionary ViewData
+    {
+        get;
+        private set;
+    }
+    public ViewContext ViewContext
+    {
+        get;
+        private set;
+    }
+
+    public IHtmlString GetMyName()
+    {
+        var tagBuilder = new TagBuilder("span");
+        tagBuilder.SetInnerText("Hello World");
+        return new HtmlString(tagBuilder.ToString());
+    }
+}
