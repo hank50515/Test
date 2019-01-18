778688217e278d105401f990b83f00faddc08a60
hank_lin <hank_lin@mail.gss.com.tw>
2019-01-18 16:14:57 +0800

    test
    test

---
+++ A.java
@@ -0,0 +1,15 @@
+package com.gss.adm.web;
+
+public class A {
+
+	
+	public void A1(){
+		B b = new B();
+		b.B1();
+	}
+	
+	public void A2(){
+		B b = new B();
+		b.B2();
+	}
+}
---
+++ B.java
@@ -0,0 +1,16 @@
+package com.gss.adm.web;
+
+public class B {
+	
+	
+	public void B1(){
+		A a = new A();
+		a.A2();
+	}
+	
+	public void B2(){
+		A a = new A();
+		a.A1();
+	}
+	
+}
