f5b37af42d5ab302b200c2ace5d7cee3c11ff007
hank_lin <hank_lin@mail.gss.com.tw>
2019-02-19 10:25:03 +0800

    test
    test

--- methodReference/Math.java
+++ methodReference/Math.java
@@ -1,7 +1,16 @@
 package com.gss.adm.antlr.java;
 
+import java.util.Comparator;
+
 public class Math {
+	
+	public static Sum sum;
+	
 	public static int addOne(int i){
 		return i + 1;
+	}
+	
+	public static int comparator(int a, int b){
+		return 0;
 	}
 }
--- methodReference/MethodReference.java
+++ methodReference/MethodReference.java
@@ -3,8 +3,10 @@ package com.gss.adm.antlr.java.listener;
 import java.util.List;
 import java.util.Objects;
 
+import com.google.common.base.Predicate;
 import com.google.common.collect.Lists;
 import com.gss.adm.antlr.java.Math;
+import com.gss.adm.antlr.java.Project;
 
 public class MethodReference {
 	public static void main(String args[]) {
@@ -14,5 +16,19 @@ public class MethodReference {
 		numbers.stream().forEach(System.out::println);
 		numbers.stream().forEach(Objects::nonNull);
 		numbers.stream().forEach(Math::addOne);
+		numbers.stream().forEach(com.gss.adm.antlr.java.Math::addOne);
+		
+		numbers.sort(Math::comparator);
+		
+		numbers.sort(Math.sum::sum);
+		
+		numbers.sort(Math.sum.aggregate::aggregateAll);
+		
+		numbers.sort(Math.sum.aggregate.account::aggregateAccount);
+		
+		Predicate<Project<String>> predicates = com.gss.adm.antlr.java.Project<String>::isEmpty;
+		
+		Predicate<Project<String>> predicates2 = Project<String>::isEmpty;
+		
 	}
 }
