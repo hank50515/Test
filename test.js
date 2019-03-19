bc7a6944cf3517a2e58f860252ef3714ae564392
hank_lin <hank_lin@mail.gss.com.tw>
2019-03-19 10:36:21 +0800

    test
    test

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
 <script type="text/javascript">  
 document.write("JavaScript is a simple language for javatpoint learners");
 $("#123").value();  
+
+var abc = function(){
+	return 123;
+}
 </script>  
