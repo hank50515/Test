1f051a6c7ca0c51753e45d84ef198d642673350c
hank_lin <hank_lin@mail.gss.com.tw>
2019-03-19 11:15:42 +0800

    test
    test

--- test.js
+++ test.js
@@ -3,3 +3,8 @@ $("#123++").click(function() {
   alert( "Handler for .click() called." );
 });
 
+var abc = function(){
+	$("#123++").click(function() {
+	  alert( "Handler for .click() called." );
+	});
+}
