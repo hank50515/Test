e8cf065febc0b11270e3d8f671b8cecb603e693a
hank_lin <hank_lin@mail.gss.com.tw>
2019-03-19 11:46:46 +0800

    test
    test

--- test.js
+++ test.js
@@ -1,12 +1,11 @@
 function initFrm() {
 	$('#123++').css("display", "");
 	$("#123++").val(TestInnerJSMethod());
+	$("#456").val(TestInnerJSMethod());
 	$.ajax({
 		url : "welcome/test",
 		context : document.body
 	}).done(function() {
 		$(this).addClass("done");
-		$(#123++).css("display", "");
-		$(#123++).val(TestInnerJSMethod());
 	});
 }
