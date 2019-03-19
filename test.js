7fb0b54347318be852f94f871bcd1391bf97202a
hank_lin <hank_lin@mail.gss.com.tw>
2019-03-19 11:22:31 +0800

    test
    test

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-
-$("#123++").click(function() {
-  alert( "Handler for .click() called." );
-});
-
-var abc = function(){
-	$("#123++").click(function() {
-	  alert( "Handler for .click() called." );
+function initFrm() {
+	$('#123++').css("display", "");
+	$("#123++").val(TestInnerJSMethod());
+	$.ajax({
+		url : "welcome/test",
+		context : document.body
+	}).done(function() {
+		$(this).addClass("done");
 	});
 }
