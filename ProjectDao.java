baec22ab292048e8a50224b45bae68137842a016
hank_lin <hank_lin@mail.gss.com.tw>
2020-03-09 16:01:28 +0800

    test
    test

---
+++ ProjectDao.java
@@ -0,0 +1,25 @@
+package com.gss.adm.core.prod.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+
+import com.gss.adm.api.spring.data.BaseRepository;
+import com.gss.adm.core.model.Project;
+
+public interface ProjectDao extends BaseRepository<Project, Long> {
+
+	@Query("SELECT code from Project where id = ?1")
+	String getCodeById(Long id);
+
+	Project findByCode(String code);
+
+	Project findByPropertiesJiraKey(String jiraKey);
+
+	@Query("SELECT PJ from Project PJ where PJ.projectTypeDefinition.display =true")
+	List<Project> findAllProjects();
+
+	@Query("SELECT PJ from Project PJ where PJ.projectTypeDefinition.id =2")
+	Project getCorssProject();
+
+}
