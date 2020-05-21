307ddcf08a4439346629854e2b4b44f8a22b00ab
hank50515 <hank50515@yahoo.com.tw>
2020-05-21 18:04:43 +0800

    Create 123.js

---
+++ 123.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+    
+    $("div#submenu > div > a:contains('要')").addClass("link hassub on");
+    $("div#submenu > div > div > a:contains('團')").click(function() {
+        var urlMain = location.href;
+        if(r==true){
+            self.btnSubmitLock(true);
+            $('#inviteForm').attr("action", url);
+            $('#inviteForm').submit();
+        }
+    });
+    
+    $(".ui-spinner-button").click(function() {
+        $(this).siblings("input").change();
+    });
+
+    function afterLoadAction() {
+        $("#hidAfterLoadMark").attr("value", "Y");
+    }
+    
+    
+    function InviteInputViewModel() {
+        var urlMain = location.href;
+        
+        var self = this;
+        //TODO
+
+	}
+
+    var vm = new InviteInputViewModel();
+    
+});
