4671ca3e5245070c917d6f5a3fc1ed9ac0f36a56
hank50515 <hank50515@yahoo.com.tw>
2020-10-13 16:02:14 +0800

    delete

--- Car.java
+++
@@ -1,8 +0,0 @@
-package com.gss.adm.antlr.java.listener;
-
-public class Car implements FourWheeler {
-	public void print() {
-		FourWheeler.super.print();
-		System.out.println("I am a car!");
-	}
-}
