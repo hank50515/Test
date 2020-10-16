42dc3be6e4377753715d2493aa7ae193e0b2ad4e
hank50515 <hank50515@yahoo.com.tw>
2020-10-16 11:59:04 +0800

    Update LambdaMath.java

--- LambdaMath.java
+++ LambdaMath.java
@@ -1,6 +1,15 @@
 
 public class LambdaMath {
+	
+	public int sum2(int a, int b){
+		return a + b;
+	}
+	
 	public int sum(int a, int b){
 		return a + b;
+	}
+	
+	public int sum(int a, int b, int c){
+		return a + b + c;
 	}
 }
