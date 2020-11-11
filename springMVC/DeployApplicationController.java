e6674c746256db9ef8ea4309fbe63896e180c29e
hank50515 <hank50515@yahoo.com.tw>
2020-11-11 14:42:12 +0800

    Update DeployApplicationController.java

--- springMVC/DeployApplicationController.java
+++ springMVC/DeployApplicationController.java
@@ -143,13 +143,6 @@ public class DeployApplicationController {
 		return departments;
 	}
 	
-	@ResponseBody
-	@RequestMapping(value = "{applicationNo}", method = RequestMethod.GET)
-	public DeployApplicationView getByApplicationNo(@PathVariable String applicationNo) {
-		DeployApplication deployApplication = deployApplicationService.getByApplicationNo(applicationNo);
-
-		return deployApplicationViewConverter.toView(deployApplication);
-	}
 	
 	@ResponseBody
 	@RequestMapping(value = "applicationNos", method = RequestMethod.GET)
@@ -183,6 +176,14 @@ public class DeployApplicationController {
 		}
 
 		return departments;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "{applicationNo}", method = RequestMethod.GET)
+	public DeployApplicationView getByApplicationNo(@PathVariable String applicationNo) {
+		DeployApplication deployApplication = deployApplicationService.getByApplicationNo(applicationNo);
+
+		return deployApplicationViewConverter.toView(deployApplication);
 	}
 
 	@ResponseBody
